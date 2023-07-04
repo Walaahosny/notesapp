7e5c8dd9686e6ccfe72ffedcc756b99b263528f2
walaahosny <whussien86@gmail.com>
2023-07-05 00:32:41 +0300

    second commit

---
+++ lib/homeScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Notes_Home_Screen extends StatelessWidget {
+  const Notes_Home_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
