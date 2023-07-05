5651a10b790ec7b2cb659a42e088cbcbbb349f18
walaahosny <whussien86@gmail.com>
2023-07-05 18:47:32 +0300

    8 commit add boorder to  bottom sheet by shape

--- lib/views/widgets/homeScreen.dart
+++ lib/views/widgets/homeScreen.dart
@@ -11,7 +11,9 @@ class NotesHomeScreen extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showModalBottomSheet(context: context, builder: (context){
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+              context: context, builder: (context){
             return const AddNoteBottomSheet();
           });
 
