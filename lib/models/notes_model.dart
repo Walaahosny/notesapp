62501a937893e7b0c6ece819f5b4fb148f03f38f
walaahosny <whussien86@gmail.com>
2023-07-06 01:03:04 +0300

    15 commit add enerate Type Adapter

--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -1,8 +1,32 @@
-class NoteModel{
+/*
+* the first step to use hive local database and use object
+* install package
+* then add extend HiveObject then detect typeId for class
+*and detect id for each field in class
+* note be careful id is unique for each class and each filed have different id
+*then write part"as import" to write in the file you will generate 'nameoffile.g.dart' and you should write the name of file true
+*as i said make typeAdapter for model and  part 'notes_model.g.dart' ;
+; is he name of file
+* this attention @HiveType(typeId: 0)  said to package of build runner this place has  code will  be generate
+* and the write this command in terminal flutter packages pub run build_runner build
+*
+* */
+import 'package:hive/hive.dart';
+part 'notes_model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   late final String title;
+  @HiveField(1)
   late final String subtitle;
+  @HiveField(2)
   late final String date;
+  @HiveField(3)
   late final int color;
 
-  NoteModel({required this.title,required this.subtitle,required this.date,required this.color});
+  NoteModel(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
 }
