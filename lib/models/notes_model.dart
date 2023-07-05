411712f548c707ac3f56283d2377ab258cd85970
walaahosny <whussien86@gmail.com>
2023-07-05 22:34:00 +0300

    14 commit add class model for notes

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,8 @@
+class NoteModel{
+  late final String title;
+  late final String subtitle;
+  late final String date;
+  late final int color;
+
+  NoteModel({required this.title,required this.subtitle,required this.date,required this.color});
+}
