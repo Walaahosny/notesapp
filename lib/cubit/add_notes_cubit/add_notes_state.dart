7c15ee9054d5a68968613d65a6569f211ddb0fc6
walaahosny <whussien86@gmail.com>
2023-07-06 03:31:25 +0300

    19 add cubit for addnotes and add default state i will use

---
+++ lib/cubit/add_notes_cubit/add_notes_state.dart
@@ -0,0 +1,8 @@
+abstract class AddNotesState{}
+class AddNotesInitial extends AddNotesState{}
+class AddNotesLoading extends AddNotesState{}
+class AddNotesSuccess extends AddNotesState{}
+class AddNotesFailure extends AddNotesState{final String errMessage;
+
+  AddNotesFailure(this.errMessage);}
+
