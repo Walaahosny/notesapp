e2f557023d45100dca994416d7d4b2c91be8ebce
walaahosny <whussien86@gmail.com>
2023-07-06 23:46:01 +0300

    24 add simple bloc observer used  BlocObserver helps you identify the current states of your BLoCs and Cubits, and react to state changes.

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'blocObserve/simplw-bloc_observe.dart';
 import 'constens/constes.dart';
 import 'models/notes_model.dart';
 
 void main() async {
+  Bloc.observer=SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
