dada102cda46837d40654d01a1f3d27039d8e02a
walaahosny <whussien86@gmail.com>
2023-07-06 04:55:42 +0300

    21 add Create add note Method

--- lib/cubit/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubit/add_notes_cubit/add_notes_cubit.dart
@@ -1,13 +1,25 @@
 import 'package:appnotess/models/notes_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 
+import '../../constens/constes.dart';
 import 'add_notes_state.dart';
 
-class AddNotesCubit extends Cubit<AddNotesState>{
-  AddNotesCubit(): super(AddNotesInitial());
+class AddNotesCubit extends Cubit<AddNotesState> {
+  AddNotesCubit() : super(AddNotesInitial());
 
   //make method for add node and then store in database
   // to make hive store add note you should make registerAdapter in main file this RegisterAdapter receive adapter
   //receive adapter i generated in file noteS_model.g.dart ,
-  addNote(NoteModel note){}
+  addNote(NoteModel note) async {
+    emit(AddNotesLoading());
+    try {
+      //add note to box
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNotesSuccess());
+      notesBox.add(note);
+    } catch (e) {
+      emit(AddNotesFailure(e.toString()));
+    }
+  }
 }
