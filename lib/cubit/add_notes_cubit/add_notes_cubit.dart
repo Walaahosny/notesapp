662505857021f2ba3c248e2301da7611aa0537ec
walaahosny <whussien86@gmail.com>
2023-07-07 23:06:20 +0300

    add  color to note

--- lib/cubit/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubit/add_notes_cubit/add_notes_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:appnotess/models/notes_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
@@ -7,18 +9,19 @@ import 'add_notes_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
+  Color color = const Color(0xff000000);
 
   //make method for add node and then store in database
   // to make hive store add note you should make registerAdapter in main file this RegisterAdapter receive adapter
   //receive adapter i generated in file noteS_model.g.dart ,
   addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNotesLoading());
     try {
       //add note to box
       var notesBox = Hive.box<NoteModel>(kNotesBox);
       notesBox.add(note);
       emit(AddNotesSuccess());
-
     } catch (e) {
       emit(AddNotesFailure(e.toString()));
     }
--- lib/widgets/color_listview.dart
+++ lib/widgets/color_listview.dart
@@ -1,5 +1,8 @@
+import 'package:appnotess/cubit/add_notes_cubit/add_notes_cubit.dart';
 import 'package:appnotess/widgets/color_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 class colorListView extends StatefulWidget {
   const colorListView({super.key});
 
@@ -8,8 +11,8 @@ class colorListView extends StatefulWidget {
 }
 
 class _colorListViewState extends State<colorListView> {
-  int currentindex=0;
-  List<Color> colors=const[
+  int currentindex = 0;
+  List<Color> colors = const [
     Color(0xffb5ca8d),
     Color(0xff8bb174),
     Color(0xff426b69),
@@ -21,30 +24,30 @@ class _colorListViewState extends State<colorListView> {
     Color(0xffa9f0d1),
     Color(0xfffff7f8),
     Color(0xfff9f0d1),
-
   ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       // mul of 2  because radius 38
-      height: 38*2,
+      height: 38 * 2,
       child: ListView.builder(
           itemCount: colors.length,
           scrollDirection: Axis.horizontal,
-          itemBuilder: (context,index){
-
-        return  Padding(
-          padding: EdgeInsets.symmetric(horizontal: 2.0),
-          child: GestureDetector(
-              onTap: (){
-                currentindex=index;
-                setState(() {
-
-                });
-              },
-              child: colorItem(ischeckedActive: currentindex==index, color: colors[index],)),
-        );
-      }),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 2.0),
+              child: GestureDetector(
+                  onTap: () {
+                    currentindex = index;
+                    BlocProvider.of<AddNotesCubit>(context).color=colors[index];
+                    setState(() {});
+                  },
+                  child: colorItem(
+                    ischeckedActive: currentindex == index,
+                    color: colors[index],
+                  )),
+            );
+          }),
     );
   }
 }
